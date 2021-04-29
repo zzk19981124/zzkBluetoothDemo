e6a6c6afe66e5a1aea67ad7420515a91608581ed
icegps_cky <chenkaiyang@icegps.com.cn>
2021-04-29 10:00:43 +0800

    2021-4-29

--- app/src/main/java/com/example/demobluetooth/MainActivity.java
+++ app/src/main/java/com/example/demobluetooth/MainActivity.java
@@ -4,11 +4,31 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.vise.baseble.ViseBle;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //使用蓝牙库之前，初始化
+        initBlueTooth();
+
+    }
+
+    private void initBlueTooth() {
+        //蓝牙相关配置修改
+        ViseBle.config()
+                .setScanTimeout(-1)//扫描超时时间，这里设置为永久扫描
+                .setConnectTimeout(10 * 1000)//连接超时时间
+                .setOperateTimeout(5 * 1000)//设置数据操作超时时间
+                .setConnectRetryCount(3)//设置连接失败重试次数
+                .setConnectRetryInterval(1000)//设置连接失败重试间隔时间
+                .setOperateRetryCount(3)//设置数据操作失败重试次数
+                .setOperateRetryInterval(1000)//设置数据操作失败重试间隔时间
+                .setMaxConnectCount(3);//设置最大连接设备数量
+        //蓝牙信息初始化，全局唯一，必须在应用初始化时调用
+        ViseBle.getInstance().init(this);
     }
 }
